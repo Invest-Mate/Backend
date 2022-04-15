ec1c864f0f91131bd433267275a4c528506eb32a
Sanskar Modi <modisanskar5@gmail.com>
2022-04-15 11:16:28 +0530

    User Schema updated

--- models/user.js
+++ models/user.js
@@ -5,26 +5,49 @@ const userSchema = new Schema({
     name: {
         type: String,
         trim: true,
+        required: [true, 'A user must have a name'],
+        maxlength: [40, 'A user name must have less or equal then 40 characters'],
+        minlength: [10, 'A user name must have more or equal then 10 characters']
+    },
+    userId: {
+        type: ObjectId,
+        required: true,
+    },
+    dob: {
+        type: Date,
+    },
+    address: {
+        type: String,
         required: true,
     },
     email: {
         type: String,
         trim: true,
+        unique: true,
+        match: [
+            /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+            "Please enter a valid email address",
+        ],
+    },
+    aadhar: {
+        type: String,
         required: true,
         unique: true,
     },
-    password: {
+    contact: {
         type: String,
         required: true,
-        min: 6,
-        max: 64,
+        max: 10,
+        min: 10,
     },
-    secret: {
+    ip: {
         type: String,
         required: true,
     },
-    about: {},
-    photo: String,
+    photo: {
+        type: String,
+        default: "",
+    },
 }, { timestamps: true });
 
 export default mongoose.model("User", userSchema);

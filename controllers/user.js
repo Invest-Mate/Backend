5518d69ef6fe9efd26f0b783a57be25627713a38
Sanskar Modi <modisanskar5@gmail.com>
2022-04-18 00:19:23 +0530

    Update User added

--- controllers/user.js
+++ controllers/user.js
@@ -30,3 +30,37 @@ export const createUser = async(req, res) => {
         });
     }
 };
+
+export const updateUser = async(req, res) => {
+    try {
+        // console.log("profile update req.body", req.body);
+        const data = {}; //Creating a storage to store object
+
+        if (req.body.name) {
+            data.name = req.body.name;
+        }
+        if (req.body.dob) {
+            data.dob = req.body.dob;
+        }
+        if (req.body.address) {
+            data.address = req.body.address;
+        }
+        if (req.body.aadhar) {
+            data.aadhar = req.body.aadhar;
+        }
+        if (req.body.contact) {
+            data.contact = req.body.contact;
+        }
+
+        let user = await User.findByIdAndUpdate(req.body._id, data, { new: true });
+        //In data variable the variable you are using should be same as that of model
+        // console.log('udpated user', user)
+
+        res.json(user);
+    } catch (err) {
+        if (err.code == 11000) {
+            return res.json({ error: "Duplicate username" });
+        }
+        console.log(err);
+    }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { createUser } from "../controllers/user";
+import { createUser, updateUser } from "../controllers/user";
 router.post("/create-user", createUser);
+router.put("/update-user", updateUser);
 module.exports = router;

c5ea5b9c1d5bd6d3ddc4b6cb20d53e3ecd8e74a0
Sanskar Modi <modisanskar5@gmail.com>
2022-04-24 14:31:58 +0530

    Implementing a global error handling middleware

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { readdirSync } from "fs";
-import morgan from 'morgan';
+import morgan from "morgan";
 const app = express();
 require("dotenv").config();
 mongoose
@@ -19,14 +19,28 @@ app.use(
         origin: "http://localhost:3000",
     })
 );
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // automatic reloading of routes
-readdirSync('./routes').map((r) => app.use("/api", require(`./routes/${r}`)))
-app.all('*', (req, res, next) => {
-    return res.status(400).json({
-        status: "Request Failed",
-        message: `Not able to find ${req.originalUrl} in the server!`
-    })
-})
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+app.all("*", (req, res, next) => {
+    // return res.status(400).json({
+    //     status: "Request Failed",
+    //     message: `Not able to find ${req.originalUrl} in the server!`
+    // })
+    var err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status = "fail";
+    err.statusCode = 404;
+    next(err);
+});
+//Above we introduced the error deliberately for testing
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "Error occured";
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    });
+});
+//This is a global error handling code
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
